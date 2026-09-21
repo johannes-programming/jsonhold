114db31099e3c7b29a7d70d13c5b7d2196d27b5b
Johannes Programming <johannes.programming@gmail.com>
2026-09-21 13:07:27 +0200

    dummy project

---
+++ README.rst
@@ -0,0 +1,7 @@
+========
+jsonhold
+========
+
+Each minor version has its own documentation.
+These docs can be found as rst-files the ``docs/`` directory of this project.
+They can also be viewed on the website `https://jsonhold.johannes-programming.online/ <https://jsonhold.johannes-programming.online/>`_.
---
+++ run_tests.py
@@ -0,0 +1,23 @@
+__all__: list[str] = ["main", "run"]
+
+import sys
+import unittest
+from typing import Never
+
+
+def main() -> Never:
+    sys.exit(not run().wasSuccessful())
+
+
+def run() -> unittest.TextTestResult:
+    loader: unittest.TestLoader
+    suite: unittest.TestSuite
+    runner: unittest.TextTestRunner
+    loader = unittest.TestLoader()
+    suite = loader.discover("tests")
+    runner = unittest.TextTestRunner(verbosity=2)
+    return runner.run(suite)
+
+
+if __name__ == "__main__":
+    main()
